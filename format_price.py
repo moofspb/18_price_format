a5957453c8d7a103907ce7ca70ebf3f156b5e0b1
Mikhail Kuntsevich <moofspb@gmail.com>
2017-04-10 21:57:57 +0300

    Write tests, add function format_price

--- format_price.py
+++ format_price.py
@@ -1,5 +1,23 @@
-def format_price(price):
+import argparse
+
+
+def get_arguments():
     pass
+
+
+def format_price(price):
+    if price and ',' in price:
+        price = price.replace(',', '.')
+    try:
+        if float(price) >= 0.0:
+            return '{:,.0f}'.format(float(price)).replace(',', ' ')
+        else:
+            return '0'
+    except ValueError:
+        return '0'
+    except TypeError:
+        return '0'
+
 
 if __name__ == '__main__':
     pass
--- tests.py
+++ tests.py
@@ -1,1 +1,23 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test_format_price(self):
+        self.assertEqual(format_price('3242.999999'), '3 243')
+        self.assertEqual(format_price('0'), '0')
+        self.assertEqual(format_price('-543433.52355'), '0')
+        self.assertEqual(format_price('432424234242'), '432 424 234 242')
+
+    def test_comma_instead_of_dot(self):
+        self.assertEqual(format_price('980,22'), '980')
+
+    def test_valueError(self):
+        self.assertEqual(format_price('price: 12312.3242'), '0')
+
+    def test_typeError(self):
+        self.assertEqual(format_price(None), '0')
+
+if __name__ == '__main__':
+    unittest.main()

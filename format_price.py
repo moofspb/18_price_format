fb9badf700ec7602f8a375c5ee908bc1e69b0cda
Mikhail Kuntsevich <moofspb@gmail.com>
2017-04-10 22:30:55 +0300

    Add get_arguments and print_price functions, correct tests

--- format_price.py
+++ format_price.py
@@ -2,7 +2,10 @@ import argparse
 
 
 def get_arguments():
-    pass
+    parser = argparse.ArgumentParser(
+        description='Script format input price from 3245.000000 to 3 245')
+    parser.add_argument('price', help='Price to format')
+    return parser.parse_args()
 
 
 def format_price(price):
@@ -12,12 +15,17 @@ def format_price(price):
         if float(price) >= 0.0:
             return '{:,.0f}'.format(float(price)).replace(',', ' ')
         else:
-            return '0'
+            return 'Negative price!'
     except ValueError:
-        return '0'
+        return 'Incorrect value!'
     except TypeError:
-        return '0'
+        return 'Incorrect type!'
+
+
+def print_price(raw_price):
+    print('Formatted price: {}'.format(format_price(raw_price)))
 
 
 if __name__ == '__main__':
-    pass
+    args = get_arguments()
+    print_price(args.price)
